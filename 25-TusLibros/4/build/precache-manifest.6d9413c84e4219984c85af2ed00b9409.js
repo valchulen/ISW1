9f681430fd83442a661123dc7791ae17e28d8c07
Valentin Paz Marcolla <valentin.paz@jampp.com>
2019-12-05 16:51:52 -0300

    Build React

---
+++ 25-TusLibros/4/build/precache-manifest.6d9413c84e4219984c85af2ed00b9409.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1729ec8f635f60977f145e62d06ee69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eab62d325146b05dc76",
+    "url": "/static/css/main.884a4d30.chunk.css"
+  },
+  {
+    "revision": "34f2928edadec46a7743",
+    "url": "/static/js/2.82e9c8b9.chunk.js"
+  },
+  {
+    "revision": "2eab62d325146b05dc76",
+    "url": "/static/js/main.b2e9e5ae.chunk.js"
+  },
+  {
+    "revision": "92f416338fa5b92447d6",
+    "url": "/static/js/runtime-main.79fbf0b5.js"
+  }
+]);
